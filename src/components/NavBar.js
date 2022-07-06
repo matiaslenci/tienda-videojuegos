11d66e7eb22b2b55767ac1062e321d9cd21f1556
Matias Lenci <matiaslenci@gmail.com>
2022-07-06 00:51:41 -0300

    creando navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+const NavBar = () => {
+  return (
+    <header>
+      <nav class="navbar navbar-expand-lg bg-light nav--color">
+        <div class="container-fluid">
+          <div>
+            <a class="navbar-brand" href="#">
+              <img src="./logo-gamestore.png" alt="logo videogame store" class="nav--logo"></img>
+            </a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Tienda
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Descuentos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Acerca de
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
